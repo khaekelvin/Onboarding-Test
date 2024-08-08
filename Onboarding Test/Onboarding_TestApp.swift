e3869e38a0771e6060d5ec676be65495ec1356e3
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-08-08 19:12:33 +0000

    Initial Commit

---
+++ Onboarding Test/Onboarding_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_TestApp.swift
+//  Onboarding Test
+//
+//  Created by KING on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
